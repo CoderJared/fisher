f3ac26a1dda846472ba7f1e9d0ff666f493dfae2
Jared <jaredtang@126.com>
2024-03-17 23:02:17 +0800

    Use another way to registry url route

--- fisher.py
+++ fisher.py
@@ -8,9 +8,11 @@ __author__ = "Jared"
 app = Flask(__name__)
 
 
-@app.route('/hello')
+# @app.route('/hello')
 def hello_world():
     return "Hello World!"
 
+
+app.add_url_rule('/hello', view_func=hello_world)
 
 app.run(debug=True)
